91fc583d4262de18fba3248efdb19fad2d547042
Jumarie Mocon <j_mocon@yahoo.com>
2023-07-19 17:09:14 +0800

    logs

--- git_pull.bat
+++ git_pull.bat
@@ -1,6 +1,6 @@
 cd C:\Websites\Bolinao-Account-Management
-git pull
+git pull >> C:\Websites\git_pull.log 2>&1
 cd C:\Websites\Bolinao-Account-Management\frontend
-yarn install
+yarn install >> C:\Websites\git_pull.log 2>&1
 cd C:\Websites\Bolinao-Account-Management\backend
-yarn install
+yarn install >> C:\Websites\git_pull.log 2>&1
