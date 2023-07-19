e944bff947e8b1d926395c849083f27e34453025
Jumarie Mocon <j_mocon@yahoo.com>
2023-07-19 18:35:33 +0800

    pulls

--- frontend.bat
+++ frontend.bat
@@ -1,2 +1,19 @@
 cd C:\Websites\Bolinao-Account-Management\frontend
-yarn start >> ../../frontend.log
+
+@echo off
+setlocal
+
+for /f "tokens=1-3 delims=/" %%a in ("%date%") do (
+    set "day=%%a"
+    set "month=%%b"
+    set "year=%%c"
+)
+
+set "day=%day:~0,2%"
+set "month=%month:~0,2%"
+
+set "file_name=../../%year%-%month%-%day%_frontend.log"
+
+yarn start >> "%file_name%" 2>&1
+
+endlocal
--- git_pull.bat
+++ git_pull.bat
@@ -1,6 +1,23 @@
+
+@echo off
+setlocal
+
+for /f "tokens=1-3 delims=/" %%a in ("%date%") do (
+    set "day=%%a"
+    set "month=%%b"
+    set "year=%%c"
+)
+
+set "day=%day:~0,2%"
+set "month=%month:~0,2%"
+
+set "file_name=C:\Websites\%year%-%month%-%day%_git_pull.log"
+
 cd C:\Websites\Bolinao-Account-Management
-git pull >> C:\Websites\git_pull.log 2>&1
+git pull >> "%file_name%" 2>&1
 cd C:\Websites\Bolinao-Account-Management\frontend
-yarn install >> C:\Websites\git_pull.log 2>&1
+yarn install >> "%file_name%" 2>&1
 cd C:\Websites\Bolinao-Account-Management\backend
-yarn install >> C:\Websites\git_pull.log 2>&1
+yarn install >> "%file_name%" 2>&1
+
+endlocal
